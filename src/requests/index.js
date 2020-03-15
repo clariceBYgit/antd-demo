8264f78990cdbe39787d448f9f9c1e6d24b4aeee
BY <baiyu1213abc@gmail.com>
2020-03-15 22:22:10 +0800

    使用axios获取数据以及表格的基本渲染

---
+++ src/requests/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { message } from 'antd'
+const isDev = process.env.NODE_ENV === 'development'
+console.log(isDev)
+const service = axios.create({
+    baseURL: isDev ? 'http://rap2.taobao.org:38080/app/mock/247090' : ''
+})
+
+// 拦截器
+service.interceptors.request.use((config) => {
+    // console.log(config)
+    config.data = Object.assign({}, config.data, {
+        // authToken:window.localStorage.getItem('authToken')
+        authToken:'itisauthtoken'
+    })
+    return config
+})
+
+service.interceptors.response.use((rep) => {
+    console.log(rep)
+    if (rep.data.code === 200){
+        
+        return rep.data.data
+       
+    } else {
+        // 全局处理错误
+        message.error(rep.data.errMsg)
+    }
+})
+
+export const getArticles = ( offset = 0, limited = 10) => {
+    return  service.post('/antd/articleList', {
+        offset,
+        limited
+    })
+}
--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,60 +1,107 @@
 import React, { Component } from 'react'
-import { Card, Table } from 'antd'
 
-const dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-  ];
+import { Card, Table, Tag } from 'antd'
+
+
+import { getArticles } from '../../requests'
+// 引入第三方时间日期格式化
+import moment from 'moment'
+  // 调试时
+  // window.moment = moment
   
-  const columns = [
-    {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: '操作',
-      dataIndex: 'actions',
-      key: 'actions',
-      render: () =>{
-        return <button>编辑</button>      }
-    }
-  ];
-  
+const titleDisplayMap = {
+  id: 'id',
+  title: '标题',
+  author: '作者',
+  createAt: '创建时间',
+  amount: '阅读量'
+}
 export default class ArticleList extends Component {
+  // 定义数据
+  constructor () {
+    super()
+    this.state = {
+      dataSource:[],
+      columns : [],
+      total: 0
+    }
+  }
+  createColums = (columnKeys) => {
+    return columnKeys.map(item => {
+      if( item === 'amount') {
+        return {
+          title: titleDisplayMap[item],
+          key: item,
+          render : (text, record) => {
+            // 这里是根据数字的大小做一个条件渲染
+              // 同理  可以根据职位不同显示不同的颜色
+            const { amount } =record
+          return <Tag color={ amount > 230 ? 'red' : 'green'}>{record.amount}</Tag>
+          }
+        }
+       
+    }
+    if( item === 'createAt') {
+      return {
+        title: titleDisplayMap[item],
+        key: item,
+        render : (text, record) => {
+          // 这里是根据数字的大小做一个条件渲染
+            // 同理  可以根据职位不同显示不同的颜色
+          const { createAt } = record
+        return moment(createAt).format('YYYY年MM月DD日  HH:mm:ss')
+        }
+      }
+     
+  }
+    return {
+      title: titleDisplayMap[item],
+      dataIndex: item,
+      key: item
+    }
+  })
+}
+  // 发送ajax 请求数据
+  getData = () => {
+    getArticles()
+    .then(rep => {
+      // Object.keys 返回一个所有元素为字符串的数组，
+      /*
+        var arr = ['a', 'b', 'c'];
+        console.log(Object.keys(arr)); // console: ['0', '1', '2']
+
+        // array like object
+          var obj = { 0: 'a', 1: 'b', 2: 'c' };
+        console.log(Object.keys(obj)); // console: ['0', '1', '2']
+      */
+      const columnKeys = Object.keys(rep.list[0])
+     const columns = this.createColums(columnKeys)
+      this.setState({
+        total:rep.total,
+        dataSource: rep.list,
+        columns
+      })
+    })
+  }
+
+
+  componentDidMount () {
+    this.getData()
+  }
     render() {
         return (
             <Card
-             title="Card title" 
+             title="文章列表" 
              bordered={false}
              extra={<button type="primary">导出Excel</button>}
              >
             <Table 
-                dataSource={dataSource}
-                columns={columns}
+                rowKey={record => record.id}
+                dataSource={this.state.dataSource}
+                columns={this.state.columns}
                 pagination={{
-                  // total:100,
-                  pageSize:1
+                  total:this.state.total,
+                  hideOnSinglePage: true
                 }}
             />
           </Card>
