7539a7194123e6649febc6caf4ffb1ff9e77e61b
BY <baiyu1213abc@gmail.com>
2020-04-15 16:10:40 +0800

    登录页ui的实现

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,11 +1,92 @@
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+
+import{
+    Form,
+    Input,
+    Button,
+    Checkbox,
+    Card
+} from 'antd'
+
+import { UserOutlined, LockOutlined} from '@ant-design/icons'
+
+
+import './login.less'
+
+
+
+@Form.create()
+class Login extends Component {
+   
+    
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields( (err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        })
+    }   
     render() {
+       const { getFieldDecorator } = this.props.form
+
         return (
-            <div>
-                登录页
-            </div>
+            <Card title='QX ADMIN登录'
+                className="qx-login-wrapper"
+            >
+                <Form
+                    className="login-form"
+                    onSubmit={this.handleSubmit}
+                    >
+                    <Form.Item>
+                        {
+                            getFieldDecorator('username', {
+                                rules: [{ 
+                                    required: true,
+                                    message: '用户名必须' }]
+                            })(
+
+                                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                            )
+                        }
+                    </Form.Item>
+                    <Form.Item>
+                        {
+                            getFieldDecorator('password', {
+                                rules: [{ 
+                                    required: true,
+                                    message: '密码必须' }]
+                            })(
+                                <Input
+                                    prefix={<LockOutlined className="site-form-item-icon" />}
+                                    type="password"
+                                    placeholder="用户密码"
+                                    />
+                            )
+                        }
+                    
+                    </Form.Item>
+                    <Form.Item>
+                        {
+                            getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true
+                            })(<Checkbox>记住我</Checkbox>)
+                        }
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                        </Button>
+                        
+                    </Form.Item>
+
+                  
+                </Form>
+            </Card>
         )
     }
 }
+
+
+export default  Login
